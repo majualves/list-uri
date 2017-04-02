0e321dc1edf8319ebededbcf9ecc39783cb41582
Maju Alves <mjualves.mja@gmail.com>
2017-04-02 17:50:54 -0300

    first commit

---
+++ 1177 uri.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main () {
+	
+	int t, i;
+
+	scanf ("%d", & t);
+		if (t>2 && t <50){
+		
+	for (i=0; i<1000; i++)
+	{
+		printf ("N[%d] = %d\n", i, i%t);
+			
+	}
+	
+		}
+	}
